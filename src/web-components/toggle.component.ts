b0060b1025aad156c60bef691946ca2fbb283296
Vitaly Tochilin <vitaly.tochilin@meistersoft.ru>
2020-03-19 15:20:31 +0300

    toggle component style/layout

--- src/web-components/toggle.component.ts
+++ src/web-components/toggle.component.ts
@@ -6,8 +6,62 @@ export class ToggleComponent extends LitElement {
   static get styles() {
     return css`
       :host {
-        display:block;
-        background-color:lightblue;
+        background: #d9e8eb;
+        display: flex;
+        align-items: center;
+        border-radius: 0.5rem;
+        padding: 0.25rem;
+        height: 2rem;
+      }
+
+      .btn {
+        flex: 1;
+        z-index: 1;
+        display: inline-block;
+        padding: 0.125rem 0.5rem;
+        position: relative;
+        text-align: center;
+        transition: background 600ms ease, color 600ms ease;
+      }
+
+      input[type="radio"].toggle {
+        display: none;
+      }
+
+      input[type="radio"].toggle + label {
+          cursor: pointer;
+          color: #797979;
+      }
+
+      input[type="radio"].toggle + label:after {
+        background: #87b5c1;
+        border-radius: 8px;
+        content: "";
+        height: 100%;
+        position: absolute;
+        top: 0;
+        transition: left 200ms cubic-bezier(0.77, 0, 0.175, 1);
+        width: 100%;
+        z-index: -1;
+      }
+
+      input[type="radio"].toggle.toggle-left + label:after {
+        left: 100%;
+      }
+
+      input[type="radio"].toggle.toggle-right + label:after {
+        left: -100%;
+      }
+
+      input[type="radio"].toggle:checked + label {
+        z-index: 2;
+        cursor: default;
+        color: #fff;
+        transition: color 200ms;
+      }
+
+      input[type="radio"].toggle:checked + label:after {
+        left: 0;
       }`;
   }
 
@@ -24,7 +78,10 @@ export class ToggleComponent extends LitElement {
 
   render() {
     return html`
-      <span>test</span>
+      <input id="toggle-on" class="toggle toggle-left" name="toggle" type="radio" value="0">
+      <label for="toggle-on" class="btn">on</label>
+      <input id="toggle-off" class="toggle toggle-right" name="toggle" type="radio" value="1" checked>
+      <label for="toggle-off" class="btn">off</label>
     `;
   }
 
